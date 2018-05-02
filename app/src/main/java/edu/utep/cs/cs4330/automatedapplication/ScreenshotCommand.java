31ea7658baa0d9dd325b2818a853af5f1f117356
Adrian Gonzalez <adrian_gonz@live.com>
2018-05-02 10:03:27 -0600

    Working on ScreenShotCommand (Second Approach)

--- app/src/main/java/edu/utep/cs/cs4330/automatedapplication/ScreenshotCommand.java
+++ app/src/main/java/edu/utep/cs/cs4330/automatedapplication/ScreenshotCommand.java
@@ -2,14 +2,14 @@ package edu.utep.cs.cs4330.automatedapplication;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-
+import android.graphics.Canvas;
 import android.os.Environment;
 import android.view.View;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.util.Date;
-
+import java.util.concurrent.ExecutionException;
 
 public class ScreenShotCommand extends Command {
 
@@ -31,8 +31,8 @@ public class ScreenShotCommand extends Command {
             String mPath = Environment.getExternalStorageDirectory().toString() + "/" + now + ".jpg";
 
 
-            //View v1 = getWindow().getDecorView().getRootView();
-            View v1 = getActivity().getWindow().getDecorView().getRootView();
+            View v1 = getWindow().getDecorView().getRootView();
+            //View v1 = getActivity().getWindow().getDecorView().getRootView();
             v1.setDrawingCacheEnabled(true);
             Bitmap bitmap = Bitmap.createBitmap(v1.getDrawingCache());
             v1.setDrawingCacheEnabled(false);
@@ -45,10 +45,17 @@ public class ScreenShotCommand extends Command {
             outputStream.flush();
             outputStream.close();
 
-           // openScreenshot(imageFile);
         } catch (Exception e){
             e.printStackTrace();
         }
+    }
+
+    public Bitmap screenShot(View view) {
+        Bitmap bitmap = Bitmap.createBitmap(view.getWidth(),
+                view.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        view.draw(canvas);
+        return bitmap;
     }
 }
 
