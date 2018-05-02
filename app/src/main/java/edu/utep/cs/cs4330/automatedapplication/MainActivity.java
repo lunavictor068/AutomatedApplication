46fe8e5d942267abafc09f9faa8949b22a291b94
Adrian Gonzalez <adrian_gonz@live.com>
2018-05-01 22:05:18 -0600

    test

--- app/src/main/java/edu/utep/cs/cs4330/automatedapplication/MainActivity.java
+++ app/src/main/java/edu/utep/cs/cs4330/automatedapplication/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // test
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
